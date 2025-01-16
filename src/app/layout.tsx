113511685ada32cbf111842eac514e235c1a4511
Vysakh Premkumar <84713473+tellmeY18@users.noreply.github.com>
2025-01-17 03:58:09 +0530

    fix opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,31 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Habitat Technology Group",
   description: "People's Movement for Sustainable Architecture",
+  openGraph: {
+    title: "Habitat Technology Group",
+    description: "People's Movement for Sustainable Architecture",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1920,
+        height: 900,
+        type: "image/png",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Habitat Technology Group",
+    description: "People's Movement for Sustainable Architecture",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1920,
+        height: 900,
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
